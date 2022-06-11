d9ed6e35e140760635a862ffa2b5228dc4518afb
Michal Vanek <michvanek@gmail.com>
2022-06-11 12:18:08 +0200

    fce validace

--- employees/task01/task.js
+++ employees/task01/task.js
@@ -2,21 +2,20 @@
 //@@viewOff:const
 
 //@@viewOn:helpers
-  /**
-   * Returns random number in range <min,max>
-   * 
-   * @param {number} min min value
-   * @param {number} max max value
-   * @return {number} random number
-  **/
-  function getRandom(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  } 
+/**
+ * Returns random number in range <min,max>
+ *
+ * @param {number} min min value
+ * @param {number} max max value
+ * @return {number} random number
+ **/
+function getRandom(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-
-  /**
+/**
    * slouzi na overeni vstupu, jestli ma definovane datove typy, vlastnosti a definovane rozsahy
    * 
    * @param {dtoIn} vstupni hodnoty k validaci
@@ -29,39 +28,43 @@
   }
    * @return {bool} true, false
   **/
-  function validate(dtoIn){
-
+function validate(dtoIn) {
+  if (isNan(dtoIn[count])) {
+    throw "count není číslo";
+  } else if (dtoIn[count] <= 0 && dtoIn[count] > 1000) {
+    throw "count není v platném rozmezí (1-1000)";
+  } else if (isNan(dtoIn[(age, min)])) {
+    throw "Minimální věk není číslo";
+  } else if (dtoIn[(age, min)] < 18) {
+    throw "Minimální věk není platný (menší než 18)";
+  } else if (isNan(dtoIn[(age, max)])) {
+    throw "Maximální věk není číslo";
+  } else if (dtoIn[(age, max)] > 70) {
+    throw "Maximální věk není platný (více než 70let)";
   }
+}
 
-  /**
-   * vrati workload, hodnoty 10, 20, 30 nebo 40
-   * 
-   * @return {int} 10, 20, 30 nebo 40
-  **/
-   function getRandomWorkload(){
-  
-  }
+/**
+ * vrati workload, hodnoty 10, 20, 30 nebo 40
+ *
+ * @return {int} 10, 20, 30 nebo 40
+ **/
+function getRandomWorkload() {}
 
-   /**
-   * slouzi k vytvoreni zakladu osoby
-   * 
-   * @return {Person} 
-  **/
-  function getRandomPerson()
-  {
+/**
+ * slouzi k vytvoreni zakladu osoby
+ *
+ * @return {Person}
+ **/
+function getRandomPerson() {}
 
-  }
-
-
-   /**
-   * vrati birthdate podle definovaneho veku
-   * @param {number} ageMin min value
-   * @param {number} ageMax max value
-   * @return {text} datum ve formátu ISO Date-Time YYYY-MM-DDTHH:MM:SSZ (např. 1981-10-28T23:00:00.000Z)
-  **/
-  function getRandomBirthdate(ageMin, ageMax){
-
-  }
+/**
+ * vrati birthdate podle definovaneho veku
+ * @param {number} ageMin min value
+ * @param {number} ageMax max value
+ * @return {text} datum ve formátu ISO Date-Time YYYY-MM-DDTHH:MM:SSZ (např. 1981-10-28T23:00:00.000Z)
+ **/
+function getRandomBirthdate(ageMin, ageMax) {}
 
 //@@viewOff:helpers
 
@@ -77,44 +80,40 @@
   }
  * @return {array} output data
 **/
-function main(dtoIn={}) {
-
+function main(dtoIn = {}) {
   //validace vstupu
-  if(!validate(dtoIn))
-  {
+  if (!validate(dtoIn)) {
     return;
   }
 
-  
   dtoIn = {
     count: 50,
     age: {
-      min: 18, 
-      max: 55
-    }
+      min: 18,
+      max: 55,
+    },
   };
-  
 
   //vytvoreni seznamu osob (iterace podle count)
-  for(var i = 0; i < dtoIn.count; i++){
+  for (var i = 0; i < dtoIn.count; i++) {
     let person = getRandomPerson();
-    let personWithDetails = new Person(dtoIn.age.min, dtoIn.age.max, persons.gender)
+    let personWithDetails = new Person(
+      dtoIn.age.min,
+      dtoIn.age.max,
+      persons.gender
+    );
   }
 
   //getRandomPerson a pak construktor te tridy
-  
-
 
   //zapsani seznamu osob
-
-
 
   //console.log('test');
 }
 //@@viewOff:main
 
 class Person {
-  generateDetails(ageMin, ageMax) {  
+  generateDetails(ageMin, ageMax) {
     // pracovní úvazek - číslo 10, 20, 30 nebo 40
     this.workload = getRandomWorkload();
 
@@ -122,14 +121,14 @@ class Person {
     this.birthdate = getRandomBirthdate(ageMin, ageMax);
   }
 
-  constructor(gender, name, surname){
-  // pohlaví, hodnota "male" nebo "female"
-  this.gender = gender;
+  constructor(gender, name, surname) {
+    // pohlaví, hodnota "male" nebo "female"
+    this.gender = gender;
 
-  // jméno
-  this.name = name;
+    // jméno
+    this.name = name;
 
-  // příjmení
-  this.surname = surname;
+    // příjmení
+    this.surname = surname;
   }
 }
